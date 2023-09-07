d368a5ed0610dffd1350f93b0ab3c3692b1f5e96
Eugene Cross <eugene.cross@gmail.com>
2023-09-07 11:53:05 +0100

    Day 65, basic UI

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -9,11 +9,55 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @State private var image: Image?
+    @State private var filterIntensity = 0.5
+    
+    
     var body: some View {
-        VStack {
-            Text("Hello world")
-                .padding()
+        NavigationView {
+            VStack {
+                ZStack {
+                    Rectangle()
+                        .fill(.secondary)
+                    
+                    Text("Tap to select picture")
+                        .foregroundColor(.white)
+                        .font(.headline)
+                    
+                    image?
+                        .resizable()
+                        .scaledToFit()
+                    
+                    
+                }
+                .onTapGesture {
+                    // select an image
+                }
+                
+                HStack {
+                    Text("Intensity")
+                    Slider(value: $filterIntensity)
+                }
+                .padding(.vertical)
+                
+                HStack {
+                    Button("Change Filter") {
+                        // change filter
+                    }
+                    
+                    Spacer()
+                    
+                    Button("Save", action: save)
+                }
+                
+            }
+            .padding([.horizontal, .bottom])
+            .navigationTitle("InstaFilter")
         }
+    }
+    
+    func save() {
+        
     }
 }
 
