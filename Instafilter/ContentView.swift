447a3925c6f8e20977b7a70f2e58f8a5f80d1e63
Eugene Cross <eugene.cross@gmail.com>
2023-09-07 11:26:46 +0100

    Day 64 midway with part 2, explained photo permission and setup of UIImageWriteToSavedPhotosAlbum

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -32,6 +32,16 @@ struct ContentView: View {
     func loadImage() {
         guard let inputImage = inputImage else { return }
         image = Image(uiImage: inputImage)
+        
+
+        // V old UIKit code
+        // - 1 the image to save
+        // - 2 the object (class) inherited from NSObject that will handle the method
+        // - 3 the method to be called on the class in 2.  Method name string not the method itself.
+        // SwiftUI needs this to have a #selector and @objc attribute
+        // #selector literally scans the class for the method name to look it up
+        // - 4 (not used here) - context.  Some data is returned when 3 is called.
+        UIImageWriteToSavedPhotosAlbum(inputImage, nil, nil, nil)
     }
 }
 
