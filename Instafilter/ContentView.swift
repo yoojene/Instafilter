50be9ae845be17de0bed3e716e8587e179447fe2
Eugene Cross <eugene.cross@gmail.com>
2023-09-07 11:40:44 +0100

    Day 64 end of day, reset project

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -9,37 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var image: Image?
-    @State private var inputImage: UIImage?
-    @State private var showingPicker = false
-    
     var body: some View {
         VStack {
-            image?
-                .resizable()
-                .scaledToFit()
-            
-            Button("Select image") {
-                showingPicker = true
-                
-            }
-            
-            Button("Save Image") {
-                guard let inputImage = inputImage else { return }
-                let imageSaver = ImageSaver()
-                imageSaver.writeToPhotoAlbum(image: inputImage)
-            }
+            Text("Hello world")
+                .padding()
         }
-        .sheet(isPresented: $showingPicker) {
-            ImagePicker(image: $inputImage)
-        }
-        .onChange(of: inputImage) { _ in loadImage()}
-    }
-    
-    func loadImage() {
-        guard let inputImage = inputImage else { return }
-        image = Image(uiImage: inputImage)
-        
     }
 }
 
